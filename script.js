31e3fb23be1e69f20dfb8deb77a22f03939e5b35
Thoalfeqar-gata <thoalfeqarkatajarullah@gmail.com>
2021-10-12 20:14:58 +0300

    Refactored the code to use the class syntax

--- script.js
+++ script.js
@@ -10,100 +10,102 @@ const shadow = document.querySelector(".shadow");
 
 
 let books = [];
-function Book(author, title, pages)
+class Book
 {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = false;
-    
-    //helper properties.
-    this.shown = false;
-    this.index = 0;
-}
-
-Book.prototype.compose = function()
-{
-    let book = document.createElement("div");
-    book.classList.add("book");
-    
-    let title = document.createElement("p");
-    title.classList.add("title");
-    
-    let author = document.createElement("p");
-    author.classList.add("author");
-    
-    let pages = document.createElement("p");
-    pages.classList.add("pages");
-
-    let removeButton = document.createElement("p");
-    removeButton.textContent = "×";
-    removeButton.classList.add("remove-book");
-
-    let readDiv = document.createElement("div");
-    readDiv.classList.add("book-status");
-    
-    if(!this.read)
+    constructor(author, title, pages)
     {
-        readDiv.textContent = "Not read";
-        readDiv.classList.add("book-not-read");
-    }
-    else
-    {
-        readDiv.textContent = "Read";
-        readDiv.classList.add("book-read");
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+
+        //helper attributes
+        this.shown = false;
+        this.index = 0;
     }
 
-    let wrapper = document.createElement("div");
-    wrapper.classList.add("book-options");
-    wrapper.appendChild(readDiv);
-    wrapper.appendChild(removeButton);
-
-    title.textContent = this.title + ".";
-    author.textContent = "By: " + this.author + ".";
-    pages.textContent = this.pages + " pages.";
-
-    removeButton.addEventListener("click", (event) =>
+    compose()
     {
-        localStorage.removeItem(this.title);
-        booksDisplay.removeChild(book);
-        let temp = [];
-        for(let book of books)
+        let book = document.createElement("div");
+        book.classList.add("book");
+        
+        let title = document.createElement("p");
+        title.classList.add("title");
+        
+        let author = document.createElement("p");
+        author.classList.add("author");
+        
+        let pages = document.createElement("p");
+        pages.classList.add("pages");
+    
+        let removeButton = document.createElement("p");
+        removeButton.textContent = "×";
+        removeButton.classList.add("remove-book");
+    
+        let readDiv = document.createElement("div");
+        readDiv.classList.add("book-status");
+        
+        if(!this.read)
         {
-            if(book.index != this.index)
-            {
-                temp.push(book);
-            }
-        }
-        books = temp;
-    });
-
-    readDiv.addEventListener("click", event =>
-    {
-        if(this.read == false)
-        {
-            event.target.classList.remove("book-not-read");
-            event.target.classList.add("book-read");
-            event.target.textContent = "Read";
-            this.read = true;
-            localStorage.setItem(this.title, JSON.stringify(this));
+            readDiv.textContent = "Not read";
+            readDiv.classList.add("book-not-read");
         }
         else
         {
-            event.target.textContent = "Not read";
-            event.target.style.backgroundColor = ""; 
-            this.read = false;
-            event.target.classList.remove("book-read");
-            event.target.classList.add("book-not-read");
-            localStorage.setItem(this.title, JSON.stringify(this));
+            readDiv.textContent = "Read";
+            readDiv.classList.add("book-read");
         }
-    });
-
-    book.appendChild(title);
-    book.appendChild(author);
-    book.appendChild(pages);
-    book.appendChild(wrapper);
-    return book;
+    
+        let wrapper = document.createElement("div");
+        wrapper.classList.add("book-options");
+        wrapper.appendChild(readDiv);
+        wrapper.appendChild(removeButton);
+    
+        title.textContent = this.title + ".";
+        author.textContent = "By: " + this.author + ".";
+        pages.textContent = this.pages + " pages.";
+    
+        removeButton.addEventListener("click", (event) =>
+        {
+            localStorage.removeItem(this.title);
+            booksDisplay.removeChild(book);
+            let temp = [];
+            for(let book of books)
+            {
+                if(book.index != this.index)
+                {
+                    temp.push(book);
+                }
+            }
+            books = temp;
+        });
+    
+        readDiv.addEventListener("click", event =>
+        {
+            if(this.read == false)
+            {
+                event.target.classList.remove("book-not-read");
+                event.target.classList.add("book-read");
+                event.target.textContent = "Read";
+                this.read = true;
+                localStorage.setItem(this.title, JSON.stringify(this));
+            }
+            else
+            {
+                event.target.textContent = "Not read";
+                event.target.style.backgroundColor = ""; 
+                this.read = false;
+                event.target.classList.remove("book-read");
+                event.target.classList.add("book-not-read");
+                localStorage.setItem(this.title, JSON.stringify(this));
+            }
+        });
+    
+        book.appendChild(title);
+        book.appendChild(author);
+        book.appendChild(pages);
+        book.appendChild(wrapper);
+        return book;
+    }
 }
 
 addButton.addEventListener("click", (event) =>
